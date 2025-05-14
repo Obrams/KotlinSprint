b47b6a859eb89fc6d627600db57c4c44fe4ca9cf
nikita <nickitarull@gmail.com>
2025-05-14 20:19:51 +0300

    KS-1-2 add new variables workersCount and print result

---
+++ src/main/kotlin/lesson_1/lesson_1_task2.kt
@@ -0,0 +1,10 @@
+package lesson_1
+
+fun main () {
+    val orderCount = 75
+    val thankYouMessage = "Благодарим Вас за совершённую покупку — приятно дарить Вам радость"
+    println("Количество заказов: $orderCount\n$thankYouMessage")
+
+    val workersCount = 1999
+    println("Количество сотрудников интернет-магазина: $workersCount")
+}
