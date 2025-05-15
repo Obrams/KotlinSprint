53ea2279671368ef206a57bb3fa8edcc606041ca
Озеров Никита Валерьевич <nikita.ozerov@megamarket.ru>
2025-05-15 17:06:19 +0300

    KS-2-1 find arithmetic mean

---
+++ src/main/kotlin/lesson_2/lesson_2_task1.kt
@@ -0,0 +1,10 @@
+package lesson_2
+
+fun main () {
+    val student1= 3
+    val student2= 4
+    val student3= 3
+    val student4= 5
+    var numberStudent = 4
+    println("Среднее арифмитическое:${(student1.toDouble() + student2.toDouble() + student3.toDouble() + student4.toDouble())/numberStudent}")
+}
