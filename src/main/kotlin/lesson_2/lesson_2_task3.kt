2993322ef70b57f7e2c7dec2294f567486c02aaf
Озеров Никита Валерьевич <nikita.ozerov@megamarket.ru>
2025-05-16 16:24:59 +0300

    KS-2-3 добавил константы для расчета в формуле

--- src/main/kotlin/lesson_2/lesson_2_task3.kt
+++ src/main/kotlin/lesson_2/lesson_2_task3.kt
@@ -1,13 +1,16 @@
 package lesson_2
 
+const val MINUTE_FIX = 60
+const val HOURS_FIX = 60
+
 fun main () {
     val hours = 9
     val minute = 39
     val travelTimeMinutes = 457
 
-    var totalMinutes = (hours * 60) + minute + travelTimeMinutes
-    val arrivalHour = (totalMinutes / 60) % 24
-    val arrivalMinute = totalMinutes % 60
+    var totalMinutes = (hours * MINUTE_FIX) + minute + travelTimeMinutes
+    val arrivalHour = (totalMinutes / MINUTE_FIX) % HOURS_FIX
+    val arrivalMinute = totalMinutes % MINUTE_FIX
 
     println("Прибытие в $arrivalHour:$arrivalMinute")
 }
