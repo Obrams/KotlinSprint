8d209bfbf4f45addf45b0640a31772a19379cd3e
Озеров Никита Валерьевич <nikita.ozerov@megamarket.ru>
2025-05-16 16:30:08 +0300

    KS-2-5 добавил константу для расчета в формулу

--- src/main/kotlin/lesson_2/lesson_2_task5.kt
+++ src/main/kotlin/lesson_2/lesson_2_task5.kt
@@ -4,11 +4,13 @@ import java.math.BigDecimal
 import java.math.RoundingMode
 import kotlin.math.pow
 
+const val PRESENT = 100
+
 fun main () {
     val sum = 70000
     val interestRate = 16.7
     val years = 20
-    val hardPercent = BigDecimal(sum * (1 + (interestRate / 100)).pow(years.toDouble()))
+    val hardPercent = BigDecimal(sum * (1 + (interestRate / PRESENT)).pow(years.toDouble()))
     val roundedHardPercent = hardPercent.setScale(3, RoundingMode.HALF_UP)
 
     println("Размер вклада после $years лет и под $interestRate% c cуммой $sum рублей составит: $roundedHardPercent")
