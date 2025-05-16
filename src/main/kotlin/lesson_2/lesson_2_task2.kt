267094e6b6663e0fc4656f5a3a275141b9e1c6f1
Озеров Никита Валерьевич <nikita.ozerov@megamarket.ru>
2025-05-16 16:20:57 +0300

    KS-2-2 добавил переменную для средней зп сотрудника

--- src/main/kotlin/lesson_2/lesson_2_task2.kt
+++ src/main/kotlin/lesson_2/lesson_2_task2.kt
@@ -7,8 +7,9 @@ fun main() {
     val salaryOneIntern = 20000
     val salaryManyWorkers = workersCount * salaryOneWorker
     val totalExpenses = (internCount * salaryOneIntern) + salaryManyWorkers
+    val averageSalary = totalExpenses / (workersCount + internCount)
 
     println("Расходы на зарплаты постоянных сотрудников: $salaryManyWorkers")
     println("Общие расходы после прихода стажеров: $totalExpenses")
-    println("Средняя ЗП одного сотрудника после устройства стажеров: ${totalExpenses / (workersCount + internCount)}")
+    println("Средняя ЗП одного сотрудника после устройства стажеров: $averageSalary")
 }
