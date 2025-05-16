f38ce346fdffebb9fd3b7b8fd4e7051f20c4f78e
Озеров Никита Валерьевич <nikita.ozerov@megamarket.ru>
2025-05-16 10:46:17 +0300

    KS-1-5 внес изменения под комментарии

--- src/main/kotlin/lesson_1/lesson_1_task5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task5.kt
@@ -1,11 +1,13 @@
 package lesson_1
 
+const val FIX_MINUTE = 60
+
 fun main () {
     val secondsCount = 6480
 
-    val hours = secondsCount / 3600
-    val minute = (secondsCount % 3600) /60
-    val seconds = secondsCount % 60
-
-    println("0$hours:$minute:0$seconds")
+    val hours = secondsCount / (FIX_MINUTE * FIX_MINUTE)
+    val minutes = (secondsCount % 3600) /FIX_MINUTE
+    val seconds = secondsCount % FIX_MINUTE
+    val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
+    println(formattedTime)
 }
