6cae278415b7b718287ffd12bd79dc36c74ffcaa
nikita <nickitarull@gmail.com>
2025-05-18 20:19:40 +0300

    KS-1-5 добавил новую переменную для расчета секунд

--- src/main/kotlin/lesson_1/lesson_1_task5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task5.kt
@@ -4,9 +4,9 @@ const val FIX_MINUTE = 60
 
 fun main () {
     val secondsCount = 6480
-
-    val hours = secondsCount / (FIX_MINUTE * FIX_MINUTE)
-    val minutes = (secondsCount % 3600) /FIX_MINUTE
+    val secondsFix = FIX_MINUTE * FIX_MINUTE
+    val hours = secondsCount / (secondsFix)
+    val minutes = (secondsCount % secondsFix) /FIX_MINUTE
     val seconds = secondsCount % FIX_MINUTE
     val formattedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds)
     println(formattedTime)
