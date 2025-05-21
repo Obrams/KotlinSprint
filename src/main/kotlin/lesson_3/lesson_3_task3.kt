d8cabf79278ae104c23c4f9c346492688d16c3a9
nikita <nickitarull@gmail.com>
2025-05-22 00:22:35 +0300

    KS-3-4 добавил расчет таблицы умножения

---
+++ src/main/kotlin/lesson_3/lesson_3_task3.kt
@@ -0,0 +1,21 @@
+package lesson_3
+
+fun main () {
+    print("Введите число: ")
+    val number = readln().toInt()
+    val multiplicationTable = """
+        Таблица умножения числа $number
+        $number * 1 = ${number * 1}
+        $number * 2 = ${number * 2}
+        $number * 3 = ${number * 3}
+        $number * 4 = ${number * 4}
+        $number * 5 = ${number * 5}
+        $number * 6 = ${number * 6}
+        $number * 7 = ${number * 7}
+        $number * 8 = ${number * 8}
+        $number * 9 = ${number * 9}
+      
+    """.trimIndent()
+
+    println(multiplicationTable)
+}
