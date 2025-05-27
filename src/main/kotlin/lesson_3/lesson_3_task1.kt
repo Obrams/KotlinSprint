44151186dd91931f60df651bb8202a2944951639
nikita <nickitarull@gmail.com>
2025-05-27 20:46:19 +0300

    KS-3-1 добавил форматирование строк и убрал знаки препинания

--- src/main/kotlin/lesson_3/lesson_3_task1.kt
+++ src/main/kotlin/lesson_3/lesson_3_task1.kt
@@ -1,9 +1,9 @@
 package lesson_3
 
-fun main () {
+fun main() {
     val name = "Никита"
-    val welcomeMessageMorning = ("Добрый день!")
-    val welcomeMessageEvening = ("Добрый вечер!")
+    val welcomeMessageMorning = ("Добрый день")
+    val welcomeMessageEvening = ("Добрый вечер")
     var welcomeMessage = ""
 
     welcomeMessage = "$welcomeMessageMorning, $name!"
