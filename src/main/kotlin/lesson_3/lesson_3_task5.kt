7b6e52eb182989632dcc0cbda29564dc275031b6
nikita <nickitarull@gmail.com>
2025-05-29 21:55:18 +0300

    KS-3-5 добавил в split два значения

--- src/main/kotlin/lesson_3/lesson_3_task5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task5.kt
@@ -2,11 +2,10 @@ package lesson_3
 
 fun main() {
     val move = "D2-D4;0"
-    val parts = move.split(";")
-    val fromTo = parts[0].split("-")
-    val from = fromTo[0]
-    val to = fromTo[1]
-    val moveNumber = parts[1]
+    val parts = move.split(";","-")
+    val from = parts[0]
+    val to = parts[1]
+    val moveNumber = parts[2]
 
     println("Ход из: $from")
     println("Ход в: $to")
