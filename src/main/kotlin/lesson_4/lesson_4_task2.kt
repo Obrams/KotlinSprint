4fed17a85864c74d755b9d50e39d6da88d3701ea
nikita <nickitarull@gmail.com>
2025-05-30 21:40:55 +0300

    KS-4-2 Добавил константы и добавил перенос

--- src/main/kotlin/lesson_4/lesson_4_task2.kt
+++ src/main/kotlin/lesson_4/lesson_4_task2.kt
@@ -1,9 +1,12 @@
 package lesson_4
+    const val MIN_WEIGHT = 35
+    const val MAX_WEIGHT = 100
+    const val MAX_VOLUME = 100
 
 fun main() {
-    val minWeight = 35
-    val maxWeight = 100
-    val maxVolume = 100
+    val minWeight = MIN_WEIGHT
+    val maxWeight = MAX_WEIGHT
+    val maxVolume = MAX_VOLUME
 
     val weight1 = 20
     val volume1 = 80
@@ -11,6 +14,8 @@ fun main() {
     val weight2 = 50
     val volume2 = 100
 
-    println("Груз с весом $weight1 кг и объемом $volume1 л соответствует категории: ${weight1 > minWeight && weight1 <= maxWeight && volume1 < maxVolume}")
-    println("Груз с весом $weight2 кг и объемом $volume2 л соответствует категории: ${weight2 > minWeight && weight2 <= maxWeight && volume2 < maxVolume}")
+    println("Груз с весом $weight1 кг и объемом $volume1 л соответствует категории: " +
+            "${weight1 > minWeight && weight1 <= maxWeight && volume1 < maxVolume}")
+    println("Груз с весом $weight2 кг и объемом $volume2 л соответствует категории: " +
+            "${weight2 > minWeight && weight2 <= maxWeight && volume2 < maxVolume}")
 }
