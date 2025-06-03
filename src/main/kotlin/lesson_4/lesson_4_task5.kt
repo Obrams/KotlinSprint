c89747f43a2fbb1ab824f66991e05f161aebed51
nikita <nickitarull@gmail.com>
2025-06-04 01:05:38 +0300

    KS-4-5 добавил константу и убрал проверку на погоду и ввод погоды тоже тк взлят от погодных условий не зависит

--- src/main/kotlin/lesson_4/lesson_4_task5.kt
+++ src/main/kotlin/lesson_4/lesson_4_task5.kt
@@ -1,6 +1,7 @@
 package lesson_4
 
-const val MIN_PEOPLE = 50
+const val MIN_PEOPLE = 55
+const val MIN_COUNT_PROVISIONS = 50
 const val MAX_PEOPLE = 70
 
 fun main() {
@@ -10,12 +11,10 @@ fun main() {
     val crewCount = readln().toInt()
     println("Сколько ящиков провизии?")
     val provisionsBoxes = readln().toInt()
-    println("Погода благоприятна? (true/false):")
-    val isWeatherFavorable = readln().toBoolean()
 
     val canSail =
-        (!hasDamage && crewCount in MIN_PEOPLE..MAX_PEOPLE && provisionsBoxes > MIN_PEOPLE) ||
-                (hasDamage && crewCount == MAX_PEOPLE && isWeatherFavorable && provisionsBoxes >= MIN_PEOPLE)
+        (!hasDamage && crewCount in MIN_PEOPLE..MAX_PEOPLE && provisionsBoxes > MIN_COUNT_PROVISIONS) ||
+                (hasDamage && crewCount == MAX_PEOPLE && provisionsBoxes >= MIN_COUNT_PROVISIONS)
 
     println("Может ли корабль отправиться в плавание? $canSail")
 }
