62c57ae6be016ee8f14546c0cb5b65a4ea9585de
nikita <nickitarull@gmail.com>
2025-06-14 00:29:55 +0300

    KS-6-2 убрал цикл и оставил только sleep

--- src/main/kotlin/lesson_6/lesson_6_task2.kt
+++ src/main/kotlin/lesson_6/lesson_6_task2.kt
@@ -2,12 +2,7 @@ package lesson_6
 
 fun main() {
     println("Введите количество секунд, которое нужно засечь:")
-    val unputSecondUser = readln().toInt()
-    var counter = 0
-
-    while (counter <= unputSecondUser) {
-        counter++
-        Thread.sleep(1000)
-    }
-    println("Прошло $unputSecondUser секунд")
+    val inputSecondUser = readln().toInt()
+    Thread.sleep(inputSecondUser * 1000L)
+    println("Прошло $inputSecondUser секунд")
 }
