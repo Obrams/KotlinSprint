4fd7bb2f47c87864c7d339946d5455e5aa13e668
nikita <nickitarull@gmail.com>
2025-07-04 00:36:32 +0300

    KS-10-3 сделал генерацию пароля через функции

---
+++ src/main/kotlin/lesson_10/lesson_10_task3.kt
@@ -0,0 +1,27 @@
+package lesson_10
+
+fun main() {
+    println("Введите длину пароля:")
+    val inputUserLeghtPassword = readln().toInt()
+
+    val password = generatePassword(inputUserLeghtPassword)
+    println("Ваш новый пароль: $password")
+
+}
+
+fun generatePassword(inputUserLeghtPassword: Int): String {
+
+    val specialChar = "!\"#\$%&'()*+,-./"
+    val numbers = ('0'..'9').joinToString("")
+
+    var passwordChar = ""
+
+    for (i in 0 until inputUserLeghtPassword) {
+        if (i % 2 == 0) {
+            passwordChar += numbers.random()
+        } else {
+            passwordChar += specialChar.random()
+        }
+    }
+    return passwordChar
+}
