9400d3158a938a29216e6f50b2652b24373fb8ac
Озеров Никита Валерьевич <nikita.ozerov@megamarket.ru>
2025-07-15 17:47:02 +0300

    KS-11-4 фикс под ревью

--- src/main/kotlin/lesson_11/lesson_11_task4.kt
+++ src/main/kotlin/lesson_11/lesson_11_task4.kt
@@ -2,26 +2,25 @@ package lesson_11
 
 class RecipeCategory(
     val id: Int,
-    val name: String,
+    val title: String,
     val imageUrl: String,
     val description: String,
-) {
-
-}
+    val recipes: List<Recipe>,
+)
 
 class Recipe(
     val id: Int,
-    val name: String,
+    val title: String,
     val imageUrl: String,
-) {
-
-}
+    val ingredients: List<Ingredient>,
+    val methodOfPreparation: String,
+)
 
 class Ingredient(
     val id: Int,
-    val name: String,
+    val title: String,
+    val count: Int,
+    val unit: String,
     val category: String,
     val imageUrl: String,
-) {
-
-}
+)
