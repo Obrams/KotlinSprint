6aef58d0d6a3ab84e73d3539557ba7f88b1390c5
nikita <nickitarull@gmail.com>
2025-07-19 13:15:24 +0300

    KS-11-5 фикс под ревью

--- src/main/kotlin/lesson_11/lesson_11_task5.kt
+++ src/main/kotlin/lesson_11/lesson_11_task5.kt
@@ -22,73 +22,32 @@ class Forum {
     fun printThread() {
         messages.forEach { message ->
             val author = members.find { it.userId == message.authorId }
-            if (author != null) {
-                println("${author.userName}: ${message.message}")
-            } else {
-                println("Неизвестный автор: ${message.message}")
-            }
+            println("${author?.userName}: ${message.message}")
         }
-
     }
 
     fun createNewMessage(authorId: Int, messageText: String) {
         val userExists = members.any { it.userId == authorId }
         if (userExists) {
-            val newMessage = ForumMessage.Builder()
-                .authorId(authorId)
-                .message(messageText)
-                .build()
+            val newMessage = ForumMessage(authorId = authorId, message = messageText)
             messages.add(newMessage)
-        } else {
-            println("Ошибка: Пользователь с ID $authorId не найден. Сообщение не создано.")
         }
     }
 
     fun createNewUser(userName: String): ForumMember {
-        val newUser = ForumMember.Builder()
-            .userId(nextUserId)
-            .userName(userName)
-            .build()
-
+        val newId = nextUserId++
+        val newUser = ForumMember(userId = newId, userName = userName)
         members.add(newUser)
-        nextUserId++
         return newUser
-
     }
 }
 
-
-class ForumMember private constructor(
+class ForumMember(
     val userId: Int,
     val userName: String,
-) {
-    class Builder {
-        private var userId: Int = 0
-        private var userName: String = ""
-        fun userId(id: Int) = apply { userId = id }
+)
 
-        fun userName(name: String) = apply { userName = name }
-
-        fun build(): ForumMember {
-            return ForumMember(userId, userName)
-        }
-
-    }
-}
-
-class ForumMessage private constructor(
+class ForumMessage(
     val authorId: Int,
     val message: String,
-) {
-    class Builder {
-        private var authorId: Int = 0
-        private var message: String = " "
-        fun authorId(id: Int) = apply { authorId = id }
-
-        fun message(text: String) = apply { message = text }
-
-        fun build(): ForumMessage {
-            return ForumMessage(authorId, message)
-        }
-    }
-}
+)
