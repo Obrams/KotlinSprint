eb9f4f6002090da716cba8f5c400152f06a392d9
nikita <nickitarull@gmail.com>
2025-07-30 01:37:44 +0300

    KS-13-3 реализовал класс фильтрацию по кампаниям без null

---
+++ src/main/kotlin/lesson_13/lesson_13_task3.kt
@@ -0,0 +1,51 @@
+package lesson_13
+
+class Contacts(
+    val name: String,
+    val phone: Long,
+    val company: String? = null
+) {
+    fun printContactInfo(name: String, phone: Long, company: String?) {
+        println(
+            "Имя: $name\n" +
+                    "Номер: $phone\n" +
+                    "Компания: ${company ?: "не указано"}\n"
+        )
+    }
+}
+
+fun main() {
+    val contacts = mutableListOf(
+        Contacts(
+            name = "Ростислав",
+            phone = 89123456789
+        ),
+        Contacts(
+            name = "Дмитрий",
+            phone = 89123456789
+        ),
+        Contacts(
+            name = "Вячеслав",
+            phone = 89123456789,
+            company = "ОЗОН"
+        ),
+        Contacts(
+            name = "Артём",
+            phone = 89123456789,
+            company = "Яндекс"
+        ),
+        Contacts(
+            name = "Гриша",
+            phone = 89123456789,
+            company = "null"
+        )
+    )
+
+    contacts.forEach { c ->
+        c.printContactInfo(c.name, c.phone, c.company)
+    }
+
+    val companies = contacts.mapNotNull { it.company }.toSet()
+
+    println(companies)
+}
