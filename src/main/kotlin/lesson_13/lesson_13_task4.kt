9ccfdd992220407bcef8ced367ca0408397d9719
nikita <nickitarull@gmail.com>
2025-08-02 00:39:57 +0300

    KS-13-4 реализовал задачу

---
+++ src/main/kotlin/lesson_13/lesson_13_task4.kt
@@ -0,0 +1,58 @@
+package lesson_13
+
+class PhoneAdress(
+    val name: String,
+    val phone: Long,
+    val company: String? = null
+) {
+    fun printContactInfo() {
+        println(
+            "Имя: $name\n" +
+                    "Номер: $phone\n" +
+                    "Компания: ${company ?: "не указано"}\n"
+        )
+    }
+}
+
+fun main() {
+    val contacts = mutableListOf<PhoneAdress>()
+
+    println("Введите имя:")
+    val name = readln()
+
+    println("Введите номер телефона:")
+    val phoneInput = readln()
+    val phone = phoneInput.toLongOrNull()
+
+    if (phone == null) {
+        println("Номер телефона не введён или введён неверно")
+    } else {
+        println("Введите компанию:")
+        val companyInput = readln()
+        val company = companyInput.ifBlank { null }
+
+        val contact = PhoneAdress(name, phone, company)
+        contacts.add(contact)
+    }
+
+    contacts.add(
+        PhoneAdress(name = "Дмитрий", phone = 89123456789)
+    )
+    contacts.add(
+        PhoneAdress(name = "Вячеслав", phone = 89123456789, company = "ОЗОН")
+    )
+    contacts.add(
+        PhoneAdress(name = "Артём", phone = 89123456789, company = "Яндекс")
+    )
+    contacts.add(
+        PhoneAdress(name = "Гриша", phone = 89123456789, company = "null")
+    )
+
+    contacts.forEach { it.printContactInfo() }
+
+    val companies = contacts
+        .mapNotNull { it.company }
+        .toSet()
+
+    println(companies)
+}
