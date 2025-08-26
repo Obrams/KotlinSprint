cbb012c5e597ec61cc34aee471d7f0f05e599c83
roman <r4633605@gmail.com>
2025-08-27 00:25:06 +0300

    KS-14-5 исправление под код ревью

--- src/main/kotlin/lesson_14/lesson_14_task5.kt
+++ src/main/kotlin/lesson_14/lesson_14_task5.kt
@@ -1,15 +1,15 @@
 package lesson_14
 
-open class Chat(
-    val chatMessage: MutableList<Message> = mutableListOf()
+class Chat(
+    private val chatMessage: MutableList<Message> = mutableListOf()
 ) {
-    var nextId = 1
+    private var nextId = 1
 
     fun addMessage(text: String, author: String) {
         val message = Message(nextId, text, author)
         chatMessage.add(message)
         nextId++
-        println("Количество сообщений:$nextId")
+        println("Количество сообщений: ${chatMessage.size}")
     }
 
     fun addThreadMessage(parentMessageId: Int, text: String, author: String) {
@@ -17,16 +17,16 @@ open class Chat(
         if (parent == null) {
             println("Родительское сообщение не найдено")
             return
-        } else {
-            val childMes = ChildMessage(
-                parentMessageId = parentMessageId,
-                id = nextId,
-                text = text,
-                author = author
-            )
-            chatMessage.add(childMes)
-            nextId++
         }
+
+        val childMes = ChildMessage(
+            parentMessageId = parentMessageId,
+            id = nextId,
+            text = text,
+            author = author
+        )
+        chatMessage.add(childMes)
+        nextId++
     }
 
     fun printChat() {
@@ -49,19 +49,17 @@ open class Message(
     val id: Int,
     val text: String,
     val author: String
-) {
-}
+)
 
 class ChildMessage(
     val parentMessageId: Int,
     id: Int, text: String,
     author: String
-) :
-    Message(
+) : Message(
         id,
         text,
         author
-    ) {}
+    )
 
 fun main() {
     val chat = Chat()
