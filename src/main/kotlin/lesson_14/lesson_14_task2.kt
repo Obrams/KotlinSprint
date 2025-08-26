0028779526b62ada301d4bdc6720379fb7aa2dec
roman <r4633605@gmail.com>
2025-08-26 23:55:32 +0300

    KS-14-2 исправление под код ревью

--- src/main/kotlin/lesson_14/lesson_14_task2.kt
+++ src/main/kotlin/lesson_14/lesson_14_task2.kt
@@ -1,24 +1,45 @@
 package lesson_14
 
+open class Liner2(
+    open val name: String,
+    open val speed: Int = 500,
+    open val loadCapacity: Int = 700,
+    open val capacity: Int = 1500,
+) {
+    open fun load() {
+        println("$name выдвигает горизонтальный трап со шкафута")
+    }
+
+    fun displayInfo() {
+        println("Название: $name")
+        println("Скорость: $speed")
+        println("Вместительность: $capacity")
+        println("Грузоподъёмность: $loadCapacity")
+        println()
+    }
+}
 
 class Cargo2(
-    name: String,
-    speed: Int = 400,
-    loadCapacity: Int = 2000,
-    capacity: Int = 500,
-) : Liner2(name, speed, loadCapacity, capacity) {
+    override val name: String
+) : Liner2(
+    name = name,
+    speed = 15,
+    loadCapacity = 500,
+    capacity = 2000
+) {
     override fun load() {
         println("$name активирует погрузочный кран")
     }
 }
 
 class Icebreaker2(
-    name: String,
-    speed: Int = 300,
-    loadCapacity: Int = 500,
-    capacity: Int = 400,
-
-    ) : Liner2(name, speed, loadCapacity, capacity) {
+    override val name: String
+) : Liner2(
+    name = name,
+    speed = 10,
+    loadCapacity = 300,
+    capacity = 300
+){
     fun chopIce() {
         println("$name колет лёд")
     }
@@ -28,37 +49,20 @@ class Icebreaker2(
     }
 }
 
-open class Liner2(
-    val name: String,
-    val speed: Int = 500,
-    val loadCapacity: Int = 700,
-    val capacity: Int = 1500,
-) {
-    open fun load() {
-        println("$name выдвигает горизонтальный трап со шкафута")
-    }
 
-    fun info() {
-        println("Название: $name")
-        println("Скорость: $speed")
-        println("Вместительность: $capacity")
-        println("Грузоподъёмность: $loadCapacity")
-        println()
-    }
-}
 
 fun main() {
     val cargo1 = Cargo2("cargo1")
     cargo1.load()
-    cargo1.info()
+    cargo1.displayInfo()
 
     val icebreaker1 = Icebreaker2("icebreaker1")
     icebreaker1.load()
     icebreaker1.chopIce()
-    icebreaker1.info()
+    icebreaker1.displayInfo()
 
     val liner1 = Liner2("liner1")
     liner1.load()
-    liner1.info()
+    liner1.displayInfo()
 
 }
