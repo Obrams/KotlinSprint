c346387143bec948b0e7e090ed3a85af8ed27fcd
roman <r4633605@gmail.com>
2025-08-27 22:04:39 +0300

    KS-15-2 фикс под ревью

--- src/main/kotlin/lesson_15/lesson_15_task2.kt
+++ src/main/kotlin/lesson_15/lesson_15_task2.kt
@@ -2,32 +2,17 @@ package lesson_15
 
 import java.sql.Timestamp
 
-/*
-Необходимо разработать простую систему мониторинга погодных условий,
-которая будет собирать данные о температуре и количестве осадков.
-
-Создай абстрактный класс WeatherStationStats.
-Он будет служить родителем для классов-объектов погодных данных: Temperature и PrecipitationAmount.
-
-Реализуй логику имитации отправки сообщения на сервер в классе WeatherServer.
- Метод отправки сообщения должен принимать объект типа WeatherStationStats и, в зависимости от типа переданного объекта,
- выводить соответствующие данные.
-
-Создай по одному объекту для разных типов погодных данных и протестируй работу программы.
- */
-
 abstract class WeatherStationStats {
     abstract val stationId: Int
     abstract val timestamp: Timestamp
     abstract fun toMessage(): String
-
 }
 
 class Temperature(
     override val stationId: Int,
     override val timestamp: Timestamp,
     val valueCelsius: Int,
-    ): WeatherStationStats(){
+) : WeatherStationStats() {
     override fun toMessage(): String {
         return ("Cтанция с номером $stationId в $timestamp передала " +
                 "температуру в $valueCelsius Цельсия")
@@ -38,7 +23,7 @@ class PrecipitationAmount(
     override val stationId: Int,
     override val timestamp: Timestamp,
     val valueMm: Long
-) : WeatherStationStats(){
+) : WeatherStationStats() {
     override fun toMessage(): String {
         return ("Cтанция с номером $stationId в $timestamp передало следующие " +
                 "количество осадков $valueMm миллиметров осадков")
@@ -46,7 +31,9 @@ class PrecipitationAmount(
 }
 
 class WeatherServer {
-    fun send(stats: WeatherStationStats) { println(stats.toMessage()) }
+    fun send(stats: WeatherStationStats) {
+        println(stats.toMessage())
+    }
 }
 
 fun main() {
