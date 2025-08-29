6f7c9e287d96539daaae5457c8cc81a2b803457e
roman <r4633605@gmail.com>
2025-08-29 23:22:56 +0300

    KS-15-5 фикс под ревью

--- src/main/kotlin/lesson_15/lesson_15_task5.kt
+++ src/main/kotlin/lesson_15/lesson_15_task5.kt
@@ -1,78 +1,74 @@
 package lesson_15
 
-import kotlin.collections.minusAssign
-
-const val MAXPASSENGERSCAR = 3
-const val MAXPASSENGERSTRUCK = 1
-const val MAXWEIGHT = 2
+const val MAX_PASSENGERS_CAR = 3
+const val MAX_PASSENGERS_TRUCK = 1
+const val MAX_WEIGHT = 2
 
 interface Movable {
     fun drive()
 }
 
 interface PassengerCarrier {
-    fun loadPassenger(count: Int)
-    fun unloadPassenger(count: Int)
-    val maxPassengers: Int
+    fun loadPassenger(count: Int) {
+        val target = (currentPassengers + count).coerceIn(0, maxPassengers)
+        currentPassengers = target
+
+    }
+
+    fun unloadPassenger(count: Int) {
+        val target = (currentPassengers - count).coerceIn(0, maxPassengers)
+        currentPassengers = target
+    }
+
+    var maxPassengers: Int
     var currentPassengers: Int
 }
 
 interface CargoCarrier {
-    fun loadCargo(amount: Int)
-    fun unloadCargo(amount: Int)
-    val maxCargo: Int
-    val currentCargo: Int
+    fun loadCargo(amount: Int) {
+        val target = (currentCargo + amount).coerceIn(0, maxCargo)
+        currentCargo = target
+    }
+
+    fun unloadCargo(amount: Int) {
+        val target = (currentCargo - amount).coerceIn(0, maxCargo)
+        currentCargo = target
+    }
+
+    var maxCargo: Int
+    var currentCargo: Int
 }
 
-class Car() : Movable, PassengerCarrier {
-    override val maxPassengers = MAXPASSENGERSCAR
+class Car : Movable, PassengerCarrier {
+    override var maxPassengers = MAX_PASSENGERS_CAR
     override var currentPassengers = 0
 
     override fun drive() {
         println("Едем")
     }
-
-    override fun loadPassenger(count: Int) {
-        when {
-            count <= 0 -> return
-            currentPassengers + count <= maxPassengers -> currentPassengers += count
-            else -> currentPassengers = maxPassengers
-        }
-    }
-
-    override fun unloadPassenger(count: Int) {
-        when {
-            count <= 0 -> return
-            currentPassengers - count >= 0 -> currentPassengers -= count
-            else -> currentPassengers = 0
-        }
-    }
 }
 
-class Truck() : Movable, PassengerCarrier, CargoCarrier {
-    override var maxCargo = MAXWEIGHT
+class Truck : Movable, PassengerCarrier, CargoCarrier {
+    override var maxPassengers = MAX_PASSENGERS_TRUCK
+    override var currentPassengers = 0
+    override var maxCargo = MAX_WEIGHT
     override var currentCargo = 0
 
     override fun drive() {
         println("Едем")
     }
+}
 
-    override fun loadPassenger(count: Int) {
-    }
-    override fun loadCargo(amount: Int) {
-        when {
-            amount <= 0 -> return
-            currentCargo + amount <= maxCargo -> currentCargo += amount
-            else -> currentCargo = maxCargo
-        }
-    }
+fun main() {
+    val car1 = Car()
+    val car2 = Car()
+    val truck = Truck()
 
-    override fun unloadCargo(amount: Int) {
-        when {
-            amount <= 0 -> return
-            currentCargo - amount >= 0 -> currentCargo -= amount
-            else -> currentCargo = 0
-        }
+    car1.loadPassenger(3)
+    car1.drive()
+    car2.loadPassenger(3)
+    car2.drive()
+    truck.loadCargo(2)
+    truck.drive()
 
-    }
 }
