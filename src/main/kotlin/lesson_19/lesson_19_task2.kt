0b4b5e403b651126bccc6dcc7d8bff1a6d22f455
Озеров Никита <nickitarull@gmail.com>
2025-09-24 21:58:20 +0300

    KS-19-2 реализация задания

---
+++ src/main/kotlin/lesson_19/lesson_19_task2.kt
@@ -0,0 +1,30 @@
+package lesson_19
+
+
+enum class CategoryType(val nameCategory: String) {
+    CLOTHING("Одежда"),
+    STATIONERY("Канцелярские товары"),
+    SUNDRY("Разное"),
+}
+
+class Product(
+    val name: String,
+    val productId: Int,
+    val categoryType: CategoryType,
+) {
+    fun printInfo() {
+        println(
+            "Название: $name\n" +
+                    "Id: $productId\n" +
+                    "Категория: ${categoryType.nameCategory}"
+        )
+    }
+}
+
+fun main() {
+    val pen = Product("РУЧКА", 1, CategoryType.STATIONERY)
+    pen.printInfo()
+
+    val shirt = Product("Футболка", 2, CategoryType.CLOTHING)
+    shirt.printInfo()
+}
